1f4aa06a4d686ca4e133c0bd17e46dcc7930951c
Aldo Ramos <katonaldin@hotmail.com>
2024-02-11 16:40:13 -0600

    Export module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import { Loading } from './Loading'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Loading />
-  </React.StrictMode>
-);
 
+
+module.exports = {
+  Loading
+}

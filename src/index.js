763f8d1d12b06b388cbe8966109586b61592a7dc
Aldo Ramos <katonaldin@hotmail.com>
2024-02-11 16:17:54 -0600

    Build loading component

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Loading } from './Loading'
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Loading />
+  </React.StrictMode>
+);
+
